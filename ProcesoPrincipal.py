a7c587b96287b707b73ca7fda9ecf52e00cab137
vickycaparelli <victoria.caparelli@gmail.com>
2019-09-29 18:11:18 -0300

    Script de prueba para testear aspectos básicos de la regla 1.1 y 1.2de la WCAG

---
+++ ProcesoPrincipal.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+from Helpers.ParseoHelper import ParseoHelper   
+from Verificacion.VerificacionCriterio01 import VerificacionCriterio01
+from Reglas import WCAGReglas
+
+resultados = list()
+url = 'https://vickycaparelli.github.io/'
+respuesta = requests.get(url)
+
+# Parsear HTML y guardar como Objeto BeautifulSoup
+contenido = BeautifulSoup(respuesta.text, "html.parser")
+
+
+# Criterio 1- Controlo textos alternativos para elementos que no sean de tipo texto
+
+for regla in WCAGReglas.dictWCAG2A_1:
+    #Parsea elementos
+    elementos = ParseoHelper.ObtenerElementos(contenido, WCAGReglas.dictWCAG2A_1[regla][1])
+
+    #Verifico regla y guardo el resultado
+    resultado_parcial = VerificacionCriterio01.Verificar(elementos, regla, WCAGReglas.dictWCAG2A_1)
+
+    #Agrego resultado a la lista de fallas
+    resultados.append(resultado_parcial)
+
+# Muestra los resultados
+for resultado in resultados:
+    print(resultado.idCriterio, resultado.Descripcion, resultado.resultados)
---
+++ ResultadoCriterio.py
@@ -0,0 +1,9 @@
+class ResultadoCriterio(object):
+    """description of class"""
+    def __init__(self, idCriterio, descripcion, resultados):
+        self.idCriterio = idCriterio
+        self.Descripcion = descripcion
+        self.resultados = resultados
+
+
+ 
