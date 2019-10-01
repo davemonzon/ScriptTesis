906beccf09caedfef18efc04a00ba25c002d69e6
Victoria Sol Caparelli <26097532+vickycaparelli@users.noreply.github.com>
2019-10-01 09:23:52 -0300

    Delete ResultadoCriterio.py

--- ScriptTesis-master/ResultadoCriterio.py
+++
@@ -1,9 +0,0 @@
-class ResultadoCriterio(object):
-    """description of class"""
-    def __init__(self, idCriterio, descripcion, resultados):
-        self.idCriterio = idCriterio
-        self.Descripcion = descripcion
-        self.elementos = resultados
-
-
- 
